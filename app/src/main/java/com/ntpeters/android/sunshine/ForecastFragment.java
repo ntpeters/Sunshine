3aade91b0ab923cfc68b82f5635d74a471eb6584
Nate Peterson <ntpeters@mtu.edu>
2014-10-19 17:42:08 -0400

    Connect fetched data to UI

--- app/src/main/java/com/ntpeters/android/sunshine/ForecastFragment.java
+++ app/src/main/java/com/ntpeters/android/sunshine/ForecastFragment.java
@@ -1,6 +1,8 @@
 package com.ntpeters.android.sunshine;
 
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -14,19 +16,26 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Date;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ForecastFragment extends Fragment {
+
+    private ArrayAdapter<String> adapter;
 
     public ForecastFragment() {
     }
@@ -58,23 +67,11 @@ public class ForecastFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        String[] forecastsArray = {
-                "Today - Sunny - 88/63",
-                "Tomorrow - Foggy - 70/40",
-                "Weds - Cloudy - 72/63",
-                "Thurs - Asteroids - 75/65",
-                "Fri - Heavy Rain - 65/56",
-                "Sat - HELP - 60/51",
-                "Sun - Sunny - 80/68"
-        };
-
-        ArrayList<String> forecasts = new ArrayList<String>(Arrays.asList(forecastsArray));
-
         FragmentActivity activity = getActivity();
         int list_item_layout_id = R.layout.list_item_forecast;
         int text_view_id = R.id.list_item_forecast_textview;
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity, list_item_layout_id, text_view_id, forecasts);
+        adapter = new ArrayAdapter<String>(activity, list_item_layout_id, text_view_id, new ArrayList<String>());
 
         ListView listView = (ListView)rootView.findViewById(R.id.list_view);
         listView.setAdapter(adapter);
@@ -82,26 +79,116 @@ public class ForecastFragment extends Fragment {
         return rootView;
     }
 
-    public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        new FetchWeatherTask().execute();
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
-        protected Void doInBackground(Void... params) {
+        private String getReadableDateString(long time) {
+            Date date = new Date(time * 1000);
+            SimpleDateFormat format = new SimpleDateFormat("E, MMM d");
+            return format.format(date).toString();
+        }
+
+        private String formatHighLows(double high, double low) {
+            long roundedHigh = Math.round(high);
+            long roundedLow = Math.round(low);
+
+            String highLowStr = roundedHigh + "/" + roundedLow;
+            return highLowStr;
+        }
+
+        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays) throws JSONException {
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_TEMP = "temp";
+            final String OWM_MAX = "max";
+            final String OWM_MIN = "min";
+            final String OWM_DATETIME = "dt";
+            final String OWM_DESC = "main";
+
+            JSONObject forecastJson = new JSONObject(forecastJsonStr);
+            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+            String[] resultStrs = new String[numDays];
+            for (int i = 0; i < weatherArray.length(); i++) {
+                String day;
+                String desc;
+                String highLow;
+
+                JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+                long dateTime = dayForecast.getLong(OWM_DATETIME);
+                day = getReadableDateString(dateTime);
+
+                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                desc = weatherObject.getString(OWM_DESC);
+
+                JSONObject tempObj = dayForecast.getJSONObject(OWM_TEMP);
+                double high = tempObj.getDouble(OWM_MAX);
+                double low = tempObj.getDouble(OWM_MIN);
+
+                highLow = formatHighLows(high, low);
+                resultStrs[i] = day + " - " + desc + " - " + highLow;
+            }
+
+            return resultStrs;
+        }
+
+        protected String[] doInBackground(String... params) {
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
             String forecastJson = null;
 
+            // Uri keys
+            final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+            final String QUERY_PARAM = "q";
+            final String FORMAT_PARAM = "mode";
+            final String UNITS_PARAM = "units";
+            final String DAYS_PARAM = "cnt";
+
+            // Default Uri values
             String zipCode = "44124";
             String responseType = "json";
             String unitType = "metric";
             int daysToRetrieve = 7;
-            String requestUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + zipCode +
-                    "&mode=" + responseType +
-                    "&units=" + unitType +
-                    "&cnt=" + daysToRetrieve;
+
+            // Load values from params if provided
+            int paramCount = params.length;
+            switch (paramCount) {
+                case 4:
+                    daysToRetrieve = Integer.parseInt(params[3]);
+                case 3:
+                    unitType = params[2];
+                case 2:
+                    responseType = params[1];
+                case 1:
+                    zipCode = params[0];
+                    break;
+                case 0:
+                    Log.v(LOG_TAG, "Using default values for request!");
+                    break;
+
+                default:
+                    Log.e(LOG_TAG, "Invalid number of arguments provided");
+            }
 
             try {
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=" + zipCode + "&mode=json&units=metric&cnt=" + daysToRetrieve);
+                // Build the Uri
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, zipCode)
+                        .appendQueryParameter(FORMAT_PARAM, responseType)
+                        .appendQueryParameter(UNITS_PARAM, unitType)
+                        .appendQueryParameter(DAYS_PARAM, Integer.toString(daysToRetrieve))
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LOG_TAG, "Built URI: " + builtUri.toString());
 
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
@@ -140,7 +227,25 @@ public class ForecastFragment extends Fragment {
                 }
             }
 
-            return null;
+            try {
+                return getWeatherDataFromJson(forecastJson, daysToRetrieve);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "Failed to convert JSON", e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            adapter.clear();
+            // Use addAll for HoneyComb or later
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                adapter.addAll(strings);
+            } else {
+                for (String entry : strings) {
+                    adapter.add(entry);
+                }
+            }
         }
     }
 }
