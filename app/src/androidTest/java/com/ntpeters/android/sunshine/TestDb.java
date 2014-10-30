208f399fb0c4408d007e8637e91140b4bc0a9bed
Nate Peterson <ntpeters@mtu.edu>
2014-10-30 00:59:35 -0400

    Add database insert test

--- app/src/androidTest/java/com/ntpeters/android/sunshine/TestDb.java
+++ app/src/androidTest/java/com/ntpeters/android/sunshine/TestDb.java
@@ -1,18 +1,47 @@
 package com.ntpeters.android.sunshine;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
+import com.ntpeters.android.sunshine.data.WeatherContract.LocationEntry;
 import com.ntpeters.android.sunshine.data.WeatherDbHelper;
 
 /**
  * Created by Nate on 10/29/2014.
  */
 public class TestDb  extends AndroidTestCase{
+    public static final String LOG_TAG = TestDb.class.getSimpleName();
+
     public void testCreateDb() throws Throwable {
         mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
         SQLiteDatabase db = new WeatherDbHelper(this.mContext).getWritableDatabase();
         assertEquals(true, db.isOpen());
         db.close();
+    }
+
+    public void testInsertReadDb() {
+        String testName = "North Pole";
+        String testLocationSetting= "99705";
+        double testLatitude = 64.772;
+        double testLongitude = -147.355;
+
+        WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(LocationEntry.COLUMN_CITY_NAME, testName);
+        values.put(LocationEntry.COLUMN_LOCATION_SETTING, testLocationSetting);
+        values.put(LocationEntry.COLUMN_COORD_LAT, testLatitude);
+        values.put(LocationEntry.COLUMN_COORD_LONG, testLongitude);
+
+        long locationRowId;
+        locationRowId = db.insert(LocationEntry.TABLE_NAME, null, values);
+
+        assertTrue(locationRowId != -1);
+        Log.d(LOG_TAG, "New row id: " + locationRowId);
+
+        
     }
 }
