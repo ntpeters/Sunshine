2d75dc6d261d8c16c196e6e317c80be561e562a2
Nate Peterson <ntpeters@mtu.edu>
2014-10-29 22:55:23 -0400

    Add initial test suite

--- app/src/androidTest/java/com/ntpeters/android/sunshine/ApplicationTest.java
+++ app/src/androidTest/java/com/ntpeters/android/sunshine/ApplicationTest.java
@@ -2,11 +2,19 @@ package com.ntpeters.android.sunshine;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+    public static Test suite() {
+        return new TestSuiteBuilder(ApplicationTest.class).includeAllPackagesUnderHere().build();
+    }
+
+
     public ApplicationTest() {
         super(Application.class);
     }
