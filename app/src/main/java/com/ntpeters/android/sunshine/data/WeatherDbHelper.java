d786183744534c207eb101234faa9ee023f92ca4
Nate Peterson <ntpeters@mtu.edu>
2014-10-29 22:51:59 -0400

    Implement onUpgrade in the database helper

--- app/src/main/java/com/ntpeters/android/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/com/ntpeters/android/sunshine/data/WeatherDbHelper.java
@@ -71,6 +71,8 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + LocationEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WeatherEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
     }
 }
