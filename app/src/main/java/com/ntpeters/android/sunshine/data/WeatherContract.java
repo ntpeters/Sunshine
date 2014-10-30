fa6b4a41339d49b89ca0d6982fcfcf04f50b6d76
Nate Peterson <ntpeters@mtu.edu>
2014-10-29 21:53:16 -0400

    Add location entry to data model contract

--- app/src/main/java/com/ntpeters/android/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/ntpeters/android/sunshine/data/WeatherContract.java
@@ -53,4 +53,15 @@ public class WeatherContract {
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
     }
+
+    public static final class LocationEntry implements BaseColumns {
+        public static final String TABLE_NAME = "location";
+
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        public static final String COLUMN_CITY_NAME = "city_name";
+
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LONG = "coord_long";
+    }
 }
